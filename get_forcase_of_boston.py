3dd150c8b2ff10fd2ae99c72cfbb32e3e9051c1d
amk09 <98000655+amk09@users.noreply.github.com>
2023-04-06 21:16:52 -0400

    .

--- get_forcase_of_boston.py
+++ get_forcase_of_boston.py
@@ -2,8 +2,9 @@ import requests
 #Boston location
 latitude = 42.36
 longitude = -71.06
+unit = 'fahrenheit'
 #url
-url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&temperature_unit={unit}"
 #api call:
 response = requests.get(url)
 #retrieve the daily temperature forecast from the JSON response using dictionary indexing, and print it to the console
