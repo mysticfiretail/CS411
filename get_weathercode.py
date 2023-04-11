e39fa88c916b94f1892a64db4e2ff185b4522894
amk09 <98000655+amk09@users.noreply.github.com>
2023-04-11 15:27:55 -0400

    added get_weathercode

---
+++ get_weathercode.py
@@ -0,0 +1,48 @@
+import requests
+weather_dict = {
+    0: 'Clear sky',
+    1: 'Mainly clear',
+    2: 'Partly cloudy',
+    3: 'Overcast',
+    45: 'Fog and depositing rime fog',
+    48: 'Fog and depositing rime fog',
+    51: 'Drizzle: Light intensity',
+    53: 'Drizzle: Moderate intensity',
+    55: 'Drizzle: Dense intensity',
+    56: 'Freezing Drizzle: Light intensity',
+    57: 'Freezing Drizzle: Dense intensity',
+    61: 'Rain: Slight intensity',
+    63: 'Rain: Moderate intensity',
+    65: 'Rain: Heavy intensity',
+    66: 'Freezing Rain: Light intensity',
+    67: 'Freezing Rain: Heavy intensity',
+    71: 'Snow fall: Slight intensity',
+    73: 'Snow fall: Moderate intensity',
+    75: 'Snow fall: Heavy intensity',
+    77: 'Snow grains',
+    80: 'Rain showers: Slight intensity',
+    81: 'Rain showers: Moderate intensity',
+    82: 'Rain showers: Violent intensity',
+    85: 'Snow showers: Slight intensity',
+    86: 'Snow showers: Heavy intensity',
+    95: 'Thunderstorm: Slight or moderate',
+    96: 'Thunderstorm with hail: Slight intensity',
+    99: 'Thunderstorm with hail: Heavy intensity'
+}
+
+def get_wc(la = 42.36,lo=-71.06, unit = 'fahrenheit'): #use boston location by default
+    #url
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={la}&longitude={lo}&daily=weathercode,temperature_2m_max,hourly=temperature_2m&temperature_unit={unit}"
+    #api call:
+    response = requests.get(url)
+    #retrieve the daily temperature forecast from the JSON response using dictionary indexing, and print it to the console
+    if response.status_code == 200:
+        data = response.json()
+        weathercodes = data['daily']['weathercode']
+        n = []
+        for x in weathercodes:
+            n.append(weather_dict[x])
+        return n
+    else:
+        print(f"Error: {response.status_code}")
+
