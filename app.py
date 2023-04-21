735b3d473f1562ee17ac8a72dece395fa99a4ea0
amk09 <98000655+amk09@users.noreply.github.com>
2023-04-21 12:32:47 -0400

    changed app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flaskext.mysql import MySQL
 import flask_login
 import json
 import requests
-
+from get_weathercode import get_wc
+#from loc import get_location
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -27,18 +28,24 @@ users = cursor.fetchall()
 def get_tem(unit = 'fahrenheit'): #use boston location by default
     la = request.form.get('latitude')
     lo = request.form.get('longtitude')
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={la}&longitude={lo}&hourly=temperature_2m&temperature_unit={unit}"
+    #url = f"https://api.open-meteo.com/v1/forecast?latitude={la}&longitude={lo}&hourly=temperature_2m&temperature_unit={unit}"
     #api call:
-    response = requests.get(url)
+    #response = requests.get(url)
     #retrieve the daily temperature forecast from the JSON response using dictionary indexing, and print it to the console
-    if response.status_code == 200:
+    #if response.status_code == 200:
         #data = response.json()
         #parsed = json.load(response.text)
-        pretty_json = json.dumps(response.json()['hourly']['temperature_2m'], indent=4, sort_keys=True)
+        #pretty_json = json.dumps(response.json()['hourly']['temperature_2m'], indent=4, sort_keys=True)
         #hourly_data = data['hourly']['temperature_2m']
-        return pretty_json
+        #return pretty_json
+    #else:
+        #print(f"Error: {response.status_code}")
+    wc = get_wc(la,lo)
+    if wc is not None:
+        m = json.dumps(wc, indent=4, sort_keys=True)
+        return m
     else:
-        print(f"Error: {response.status_code}")
+        return json.dumps("Oops, some errors occured", indent=4, sort_keys=True)
 
 
 @app.route('/logout')
