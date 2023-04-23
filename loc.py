06ce32472e602e240e537c0fdbe26cb0ddb92ff8
amk09 <98000655+amk09@users.noreply.github.com>
2023-04-23 14:13:15 -0400

    added examples on loc.py

--- loc.py
+++ loc.py
@@ -3,6 +3,9 @@ from geopy.exc import GeocoderTimedOut
 #If you get geopy installed, this will return you the latitude and longitude.
 #Just use pip install geopy, it will work!!
 def get_lat_long(city):
+    #convert it to lower cases, make it more efficient
+    city = city.lower()
+    print(city)
     geolocator = Nominatim(user_agent="my_app")
     try:
         location = geolocator.geocode(city, timeout=10)
@@ -12,3 +15,8 @@ def get_lat_long(city):
             return location.latitude, location.longitude
     except GeocoderTimedOut:
         return None
+#example
+#not case sensitive, but performs better if they are all lower cases
+print(get_lat_long('BostOn'))
+print(get_lat_long('new york'))
+print(get_lat_long('xiamen'))
