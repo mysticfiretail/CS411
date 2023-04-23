e5bd0b705a3db156574f3a11153c34cb3775c808
amk09 <98000655+amk09@users.noreply.github.com>
2023-04-23 10:31:03 -0400

    .

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import flask
-from flask import Flask, Response, request, render_template, redirect, url_for
+from flask import Flask, Response, request, render_template, redirect, url_for, g
 from flaskext.mysql import MySQL
 import flask_login
 import json
 import requests
 from get_weathercode import get_wc
+import sqlite3
+
 #from loc import get_location
 
 mysql = MySQL()
@@ -13,16 +15,68 @@ app.secret_key = 'super secret string'  # Change this!
 
 #These will need to be changed according to your creditionals
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Mysqlajue666'
 app.config['MYSQL_DATABASE_DB'] = 'photoshare'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-conn = mysql.connect()
-cursor = conn.cursor()
-cursor.execute("SELECT email from Users")
-users = cursor.fetchall()
+#conn = mysql.connect()
+#cursor = conn.cursor()
+#cursor.execute("SELECT email from Users")
+#users = cursor.fetchall()
+# connect to the GeoNames SQLite database
+# configure the database path
+DATABASE = 'geonames.db'
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_db(error):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def index():
+    # retrieve a list of all countries from the database
+    cursor = get_db().cursor()
+    cursor.execute("SELECT DISTINCT country_name FROM geonames ORDER BY country_name")
+    countries = cursor.fetchall()
+
+    return render_template('index.html', countries=countries)
+
+@app.route('/getCities', methods=['POST'])
+def getCities():
+    # retrieve the selected country from the request data
+    country = request.form['country']
+
+    # retrieve a list of all cities in the selected country
+    cursor = get_db().cursor()
+    cursor.execute("SELECT city_name, latitude, longitude FROM geonames WHERE country_name=?", (country,))
+    cities = cursor.fetchall()
+
+    # return the list of cities as a JSON response
+    return {'cities': cities}
+#@app.route('/getLocation', methods=['POST'])
+#def getLocation():
+    # get user's IP address
+    #ip = request.remote_addr
+    
+    # call an external API to get the user's location from their IP address
+    #response = requests.get('http://ip-api.com/json/'+ip)
+    #data = response.json()
+    
+    # extract latitude and longitude from the response
+    #lat = data['lat']
+   # lon = data['lon']
+    
+    # return the location data as a string
+    #return json.dumps(str(lat) + ',' + str(lon), indent=4, sort_keys=True)
 
 @app.route('/weather', methods=['POST'])
 def get_tem(unit = 'fahrenheit'): #use boston location by default
--- loc.py
+++ loc.py
@@ -1,14 +1,7 @@
 from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut
-#still not working, supposed to get geopy installed.
+
 def get_location():
-    try:
-        geolocator = Nominatim(user_agent="my-application")
-        ip_address = request.remote_addr  # get the user's IP address
-        location = geolocator.geocode(ip_address)
-        latitude = location.latitude
-        longitude = location.longitude
-        return (latitude, longitude)
-    except GeocoderTimedOut:
-        return None
+    geolocator = Nominatim(user_agent="my-app/1.0")
+    location = geolocator.geocode("your query here")
+    return location
 m = get_location()
